8a8b8488f68bb33df7869e7b5969b59c7955335f
attTuo <tuomaalanatte@gmail.com>
2024-03-12 21:21:53 +0200

    group size basic ui, remove tab

--- discScore/app/(tabs)/index.tsx
+++ discScore/app/(tabs)/index.tsx
@@ -1,12 +1,48 @@
-import { StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 import { Text, View } from '@/components/Themed';
 
 export default function TabIndexScreen() {
+
+  const [groupSize, setGroupSize] = useState<Number>(0);
+
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Choose group size</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-    </View>
+
+      { (groupSize == 0)
+      ? <View style={styles.container}>
+          <Text style={styles.title}>Choose group size</Text>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+          <View style={styles.boxholder}>
+            <Pressable style={styles.box}>
+              <Text style={styles.largeNumber}>1</Text>
+            </Pressable>
+            <Pressable style={styles.box}>
+              <Text style={styles.largeNumber}>2</Text>
+            </Pressable>
+            <Pressable style={styles.box}>
+              <Text style={styles.largeNumber}>3</Text>
+            </Pressable>
+            <Pressable style={styles.box}>
+              <Text style={styles.largeNumber}>4</Text>
+            </Pressable>
+            <Pressable style={styles.box}>
+              <Text style={styles.largeNumber}>5</Text>
+            </Pressable>
+            <Pressable style={styles.box}>
+              <Text style={styles.largeNumber}>+</Text>
+            </Pressable>
+          </View>
+        </View>
+
+      : <View style={styles.container}>
+          <Text style={styles.title}>Scoreboard</Text>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        </View>
+      }
+    </View>  
   );
 }
 
@@ -17,12 +53,37 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: 'bold',
+    marginTop: 20
   },
   separator: {
-    marginVertical: 30,
+    marginTop: 30,
+    marginBottom: 10,
     height: 1,
     width: '80%',
   },
+  boxholder: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    flexWrap: 'wrap',
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  box: {
+    width: 150,
+    height: 150,
+    padding: 10,
+    margin: 20,
+    borderColor: 'grey',
+    borderWidth: 5,
+    alignContent: 'center',
+  },
+  largeNumber: {
+    textAlign: 'center',
+    fontSize: 80,
+    fontWeight: 'bold',
+  }
 });
