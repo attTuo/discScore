c585c49d403c749fae5a3839bcf3b2e36b1e2109
attTuo <tuomaalanatte@gmail.com>
2024-03-26 18:59:02 +0200

    Added - functionality to roulette page

--- discScore/app/(tabs)/roulette.tsx
+++ discScore/app/(tabs)/roulette.tsx
@@ -1,10 +1,42 @@
+import React, { useState } from 'react';
 import { Pressable, StyleSheet } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Text, View } from '@/components/Themed';
 
 export default function TabRouletteScreen() {
 
+  const shotArray: string[] = [
+    'Backhand',
+    'Forehand'
+  ];
+  const shapeArray: string[] = [
+    'Hyzer',
+    'Anhyzer',
+    'Roller',
+  ];
+  const discArray: string[] = [
+    'a Putter',
+    'a Midrange',
+    'a Fairway Driver',
+    'a Distance Driver',
+    'the most UNDERSTABLE disc in your bag',
+    'the most OVERSTABLE disc in your bag'
+  ];
 
+  const [shot, setShot] = useState<string>('');
+  const [shape, setShape] = useState<string>('');
+  const [disc, setDisc] = useState<string>('');
+
+  const rollRandom = (): void => {
+
+    setShot(shotArray[ getRandomInt(shotArray.length) ]);
+    setShape(shapeArray[ getRandomInt(shapeArray.length) ]);
+    setDisc(discArray[ getRandomInt(discArray.length) ]);
+  }
+
+  const getRandomInt = (arraySize: number): number => {
+    return Math.floor(Math.random() * arraySize);
+  }
 
   return (
     <View style={styles.container}>
@@ -15,24 +47,29 @@ export default function TabRouletteScreen() {
       <View style={styles.rouletteContent}>
 
         <FontAwesome style={styles.icon} name='dashboard'/>
+        
+        { (!shot && !shape && !disc)
+        
+          ? <View>
+              <Text style={styles.text}>Press the button for a randomized shot!</Text>
+            </View>
 
-        <Text style={styles.title}>Throw</Text>
+          : <View style={styles.rouletteContent}>
+               <Text style={styles.title}>Throw</Text>
+                <Text style={styles.text}>{shot} - {shape}</Text>
+                <Text style={styles.title}>With</Text>
+                <Text style={styles.text}>{disc}</Text>
+            </View>  
+        }
 
-        <Text style={styles.text}>Shot variable - Shape variable</Text>
-
-        <Text style={styles.title}>With</Text>
-
-        <Text style={styles.text}>Disc variable</Text>
-
-        <Pressable style={styles.rollButton}>
-
+        <Pressable 
+          style={styles.rollButton}
+          onPress={rollRandom}
+        >
           <View style={styles.buttonContent}>
-
             <Text style={styles.buttonText}>Roll</Text>
             <FontAwesome style={styles.smallIcon} name='refresh'/>
-
           </View>
-
         </Pressable>
 
       </View>
@@ -44,8 +81,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 100
+    justifyContent: 'center'
   },
   title: {
     fontSize: 35,
@@ -57,7 +93,6 @@ const styles = StyleSheet.create({
     width: '80%',
   },
   rouletteContent: {
-    flex: 1,
     alignItems: 'center'
   },
   text: {
@@ -74,7 +109,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: 'black',
     marginTop: 50,
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   buttonContent: {
     flex: 1,
