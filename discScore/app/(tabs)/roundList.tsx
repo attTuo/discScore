b5760379e79b8061e5fa9bd88554411ea5f6ecd6
attTuo <tuomaalanatte@gmail.com>
2024-05-29 17:16:27 +0300

    Added - Saved course delete confirmation alert

--- discScore/app/(tabs)/roundList.tsx
+++ discScore/app/(tabs)/roundList.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Pressable, StyleSheet, ScrollView } from 'react-native';
+import { Alert, Pressable, StyleSheet, ScrollView } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import { removeRound, getAllSavedRounds, clearAll, StorageResult, PlayerScore } from '../storage';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
@@ -27,11 +27,29 @@ export default function TabRoundsScreen() {
     
   }, []);
 
+	const createAlert = (key : string) => {
+
+    Alert.alert('Delete round', `Are you sure want to delete the round? Round played: ${key}`, [
+      {
+        text: 'Delete', 
+        onPress: () => {
+          removeRound(key);
+          fetchSavedRounds();
+        }
+      },
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+    ]);
+  }
+
   return (
 
     <View style={styles.container}>
 
-			<View style={{flexDirection: 'row', paddingTop: 10, paddingHorizontal: 20,alignSelf: 'center', backgroundColor: '#FAF9F6', paddingBottom: 10}}>
+			<View style={styles.topContent}>
+				
 				<Text style={styles.title}>Saved Rounds</Text>
 
 				<Pressable
@@ -45,11 +63,13 @@ export default function TabRoundsScreen() {
 				{ (data !== undefined)
 
 					?	<ScrollView style={styles.scrollBox}>
+
 							{data?.map( ( round: StorageResult, idx: number) => (
 						
 								<View style={styles.listItem} key={idx}>
 									
 									<View style={styles.upperInfo}>
+
 										<Text style={styles.courseName}>{round.value.courseName}</Text>
 
 										<View style={styles.dateTimeInfo}>
@@ -73,7 +93,7 @@ export default function TabRoundsScreen() {
 									<Pressable style={styles.removeButton}
 										onPress={() => {
 											data.slice(idx - 1);
-											removeRound(data[idx].key);
+											createAlert(data[idx].key);
 											fetchSavedRounds();
 										}}
 									>
@@ -82,13 +102,14 @@ export default function TabRoundsScreen() {
 								</View>
 							))}
 						</ScrollView>
+
 					: <View style={styles.listItem}>
 							<View style={styles.flexRow}>
-								<Text style={styles.playerName}>Forest course</Text>
-								<Text style={styles.playerName}>Jack</Text>
-								<Text style={styles.playerName}>Score: 7</Text>
-								<Text>12.12.2024</Text>
-								<Text>12:15</Text>
+								<Text style={styles.playerName}>Example Course</Text>
+								<Text style={styles.playerName}>John</Text>
+								<Text style={styles.playerName}>Score: 0</Text>
+								<Text>12.12.2012</Text>
+								<Text>12:12</Text>
 							</View>
 						
 							<Pressable style={styles.removeButton}
@@ -116,6 +137,14 @@ const styles = StyleSheet.create({
     flex: 1,
 		backgroundColor: '#FAF9F6',
 	},
+	topContent: {
+		flexDirection: 'row',
+		paddingTop: 10, 
+		paddingHorizontal: 20,
+		alignSelf: 'center', 
+		backgroundColor: '#FAF9F6', 
+		paddingBottom: 10
+	},
 	title: {
 		fontSize: 35,
 		fontWeight: 'bold',
@@ -131,7 +160,8 @@ const styles = StyleSheet.create({
 		borderRadius: 10,
 		flex: 1,
 		alignItems: 'center',
-		backgroundColor: '#4361ee'
+		backgroundColor: '#4361ee',
+		elevation: 5
 	},
 	scrollBox: {
 		paddingHorizontal: 20,
